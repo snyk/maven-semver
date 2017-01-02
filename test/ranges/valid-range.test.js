17b20980b7ffec37748018d3b09f4df4cf0937a0
Dror Ben-Gai <dror@snyk.io>
2017-01-03 01:23:07 +0200

    feat:maven-semver - supporting range; missing some negative tests

--- test/ranges/valid-range.test.js
+++ test/ranges/valid-range.test.js
@@ -5,23 +5,24 @@ import { validRange } from '../../';
 // validRange(range): Return the valid range or null if it's not valid
 
 test('validRange(range)', t => {
-  t.deepEqual(validRange('1.0'), ['=>', '1.0']);
-  t.deepEqual(validRange('[1.0]'), ['==', '1.0']);
-  // t.deepEqual(validRange('(1.0,]'), ['>', '1.0']);
-  // t.deepEqual(validRange('(,1.0]'), ['<=', '1.0']);
-  // t.deepEqual(validRange('(1.0,2.0]'), ['>', '1.0', '<=', '2.0']);
+  t.deepEqual(validRange('(1.0,]'), [['>', '1.0']]);
+  t.deepEqual(validRange('1.0'), [['>=', '1.0']]);
+  t.deepEqual(validRange('[1.0]'), [['==', '1.0']]);
+  t.deepEqual(validRange('[1.2,1.3]'), [['>=', '1.2', '<=', '1.3']]);
+  t.deepEqual(validRange('[1.0,2.0)'), [['>=', '1.0', '<', '2.0']]);
+  t.deepEqual(validRange('[1.5,)'), [['>=', '1.5']]);
+  t.deepEqual(validRange('(,1.0],[1.2,)'), [['<=', '1.0'], ['>=', '1.2']]);
+  t.deepEqual(validRange('(,1.1),(1.1,)'), [['<', '1.1'], ['>', '1.1']]);
 
-  // t.is(validRange('(1.0'), null);
-  // t.is(validRange('[1.0'), null);
-  // t.is(validRange('1.0)'), null);
-  // t.is(validRange('1.0]'), null);
 
-  // t.is(validRangeRange('1.1'), '= 1.1');
-  // t.is(validRangeRange('~> 1.1'), '< 2, >= 1.1');
-  // t.is(validRangeRange('~> 1.1.0'), '< 1.2, >= 1.1.0');
-  // t.is(validRangeRange('~> 1.1.1.0'), '< 1.1.2, >= 1.1.1.0');
-  // t.is(validRangeRange('~> 1.1.1.beta.1'), '< 1.2, >= 1.1.1.beta.1');
-  // t.is(validRangeRange('> 2.1, < 2.4'), '< 2.4, > 2.1');
+  t.deepEqual(validRange('[1.0,)'), [['>=', '1.0']]);
+  t.deepEqual(validRange('(,1.0]'), [['<=', '1.0']]);
+  t.deepEqual(validRange('(1.0,2.0]'), [['>', '1.0', '<=', '2.0']]);
+
+  // t.deepEqual(validRange('(1.0'), []);
+  // t.deepEqual(validRange('[1.0'), []);
+  // t.deepEqual(validRange('1.0)'), []);
+  // t.deepEqual(validRange('1.0]'), []);
 
   // t.is(validRangeRange(''), '>= 0');
   // t.is(validRangeRange(), null);
