6b0b215b6ab5f3370d2c2e4824a97dab5f115a2d
Dror Ben-Gai <dror@snyk.io>
2017-01-10 15:18:21 +0200

    fix: more tests to ranges; accepts non-string semvers; returns null on invalid range

--- test/ranges/satisies.test.js
+++ test/ranges/satisies.test.js
@@ -11,10 +11,13 @@ test('satisfies(version, range)', t => {
   t.true(satisfies('1.1.5', '1.1.5'));
   t.true(satisfies('1.4.11', '[1.3,1.5)'));
   t.true(satisfies('1.2.3', '[1.2,)'));
+  t.true(satisfies('1.2.3', '(,1.0],[1.2,)'));
+  t.true(satisfies('0.9.0', '(,1.0],[1.2,)'));
   // oddly enough, this is correct:
   t.true(satisfies('1.2.1', 'nonsense'));
 
   t.false(satisfies('1.0', '[1.1,)'));
   t.false(satisfies('1.5.2', '[1.3,1.5)'));
+  t.false(satisfies('1.1.8', '(,1.0],[1.2,)'));
 
 });
