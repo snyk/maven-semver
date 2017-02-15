1ef171a1a0b3fba40d8fb1b2ef6b116e93003643
Dror Ben-Gai <dror@snyk.io>
2017-02-15 22:47:18 +0000

    fix: bug in range parsing, SC-2836

--- test/ranges/satisfies.test.js
+++ test/ranges/satisfies.test.js
@@ -13,6 +13,9 @@ test('satisfies(version, range)', t => {
   t.true(satisfies('1.2.3', '[1.2,)'));
   t.true(satisfies('1.2.3', '(,1.0],[1.2,)'));
   t.true(satisfies('0.9.0', '(,1.0],[1.2,)'));
+  t.false(satisfies('4.2.1', '[2.0.0,3)'));
+  t.false(satisfies('4.2.1', '[2.0.0,3),[3.0.0.RELEASE,3.1),[3.1.0.RELEASE,3.2)'));
+  t.false(satisfies('4.2.1.RELEASE', '(4.1.0.RELEASE,4.2.0.RELEASE)'));
   // oddly enough, this is correct:
   t.true(satisfies('1.2.1', 'nonsense'));
 
